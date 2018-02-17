23f3f852c0ff2032ced8867185bc0dc1229f0959
Ramon Goncalves <raigons@gmail.com>
2018-02-17 16:58:15 -0200

    Initializing rails application
      - Rails application has been initiated;
      - Some adjustments on scripts in order to properly run application into
      a docker container

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'lograge'
+gem 'rails', '~> 5.1.1'
+gem 'puma', '~> 3.9.1'
+gem 'sqlite3'
+gem 'tzinfo-data'
+gem 'listen'
--- infra/local_build.sh
+++ infra/local_build.sh
@@ -3,18 +3,33 @@ set -x
 set -e
 set -o pipefail
 
+build_image="approva-app"
+
 function build_ruby() {
+  echo "\n\n## Building Ruby image ## \n"
   IMAGE_NAME="approva/ruby:2.4.1"
 
   docker build -f infra/ruby/Dockerfile -t ${IMAGE_NAME} .
+  echo "\n## Finished building ruby image##\n\n"
 }
 
 function build_rails_app() {
-  build_image="payment-platform-gateway"
+  echo "## Building App Image ##\n\n"
+  docker build -f infra/app/Dockerfile -t ${build_image} .
+  echo "\n## Finished building App image##\n\n"
+}
+
+function run_rails_app() {
+  echo "## Running rails application ## \n\n"
   custom_log_path="/var/log/service.log"
 
-  docker build -f infra/app/Dockerfile -t $build_image .
+  docker run -d -p 8080:8080 \
+            --rm \
+            --env CUSTOM_LOG_PATH=$custom_log_path \
+            $build_image
 }
 
 #script steps
 build_ruby
+build_rails_app
+run_rails_app
